06064648b8ed70a868cc164a10570f20561e5a84
Georg <georg.stalder@gmail.com>
2023-09-18 16:33:02 +0200

    convert pdf to array and shape to readable object

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 pdfjsLib.GlobalWorkerOptions.workerSrc =
-'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
+    'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
 
 let searchTerms;
 let files;
@@ -13,49 +13,72 @@ function start() {
     console.log('files', files)
 
 
-    //start function to extract text
-    extractText(files[0].url).then(
-        function (text) {
-            console.log('parse ' + text);
-        },
-        function (reason) {
-            console.error(reason);
-        },
-        );
-        
+    convertPdfToArrayBuffer(files[0]).then(
+        (resp) => {
+            console.log(resp)
+            //shape into readable object
+            src = { data: resp };
+
+            //start function to extract text
+            extractText(src).then(
+                function (text) {
+                    console.log('parse ' + text);
+                },
+                function (reason) {
+                    console.error(reason);
+                },
+            );
+
+        }
+
+    )
+
+
 
 
 }
 
-function extractText(pdfUrl) {
-var pdf = pdfjsLib.getDocument(pdfUrl);
-return pdf.promise.then(function (pdf) {
-    var totalPageCount = pdf.numPages;
-    var countPromises = [];
-    for (
-        var currentPage = 1;
-        currentPage <= totalPageCount;
-        currentPage++
-    ) {
-        var page = pdf.getPage(currentPage);
-        countPromises.push(
-            page.then(function (page) {
-                var textContent = page.getTextContent();
-                return textContent.then(function (text) {
-                    return text.items
-                        .map(function (s) {
-                            return s.str;
-                        })
-                        .join('');
-                });
-            }),
-        );
-    }
-
-    return Promise.all(countPromises).then(function (texts) {
-        return texts.join('');
+async function convertPdfToArrayBuffer(file) {
+    console.log(" readfile function called")
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+        reader.onerror = reject;
+        reader.readAsArrayBuffer(file);
     });
-});
+}
+
+function extractText(pdfUrl) {
+    var pdf = pdfjsLib.getDocument(pdfUrl);
+    return pdf.promise.then(function (pdf) {
+        var totalPageCount = pdf.numPages;
+        var countPromises = [];
+        for (
+            var currentPage = 1;
+            currentPage <= totalPageCount;
+            currentPage++
+        ) {
+            var page = pdf.getPage(currentPage);
+            countPromises.push(
+                page.then(function (page) {
+                    var textContent = page.getTextContent();
+                    return textContent.then(function (text) {
+                        return text.items
+                            .map(function (s) {
+                                return s.str;
+                            })
+                            .join('');
+                    });
+                }),
+            );
+        }
+
+        return Promise.all(countPromises).then(function (texts) {
+            return texts.join('');
+        });
+    });
 }
 
 
